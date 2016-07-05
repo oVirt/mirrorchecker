e8175e412bf49864219332fcd3f85fd2a940c384
ngoldin <ngoldin@redhat.com>
2016-07-05 18:48:47 +0300

    allow to define specific dirs in mirrors.yaml and style fixes

--- mirror_check.py
+++ mirror_check.py
@@ -9,25 +9,24 @@ from twisted.internet import task
 from twisted.internet import reactor
 from twisted.web.wsgi import WSGIResource
 from twisted.web.server import Site
-from collections import namedtuple
-
+from paramiko.ssh_exception import SSHException
 
 defaults = {
-        'log_level': 'debug',
-        'log_file': 'mirror_checker.log',
-        'http_port': 8080,
-        'http_prefix': 'api',
-        }
-backends = None
+    'log_level': 'debug',
+    'log_file': 'mirror_checker.log',
+    'http_port': 8080,
+    'http_prefix': 'api'}
+backend = None
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
+
 
 class stamper(object):
     """
     put timestamps in directories via SCP
     """
-    def __init__(self, name, remote_path, ssh_args, dirs='', max_stamp=10,
-                 stamp_interval='60', mirrors={}, ts_fname=''):
+    def __init__(self, name, remote_path, ssh_args, mirrors, dirs=None,
+                 max_stamp=10, stamp_interval='60', ts_fname=''):
         self.name = name
         self.ssh_args = ssh_args
         self.max_stamp = max_stamp
@@ -36,8 +35,9 @@ class stamper(object):
         self.stamp_interval = stamp_interval
         self.last_stamps = {}
         self.mirrors = mirrors
-        self.mirrors_tasks = {}
+        self.mirror_tasks = {}
         self.ts_fname = ts_fname
+        self.task = None
 
     def run(self):
         ssh, proxy = utils.get_ssh(self.ssh_args)
@@ -46,18 +46,23 @@ class stamper(object):
         for stamp in self.dirs:
             path = '/'.join([self.remote_path, stamp.strip()])
             try:
-                with sftp.open(path, 'w') as f:
+                logger.debug('writing %s', path)
+                with sftp.open(path, 'w') as f_ts:
                     timestamp = utils.get_timestamp()
-                    f.write(unicode(timestamp))
+                    f_ts.write(unicode(timestamp))
                     self.last_stamps[stamp] = int(timestamp)
-            except Exception, e:
-                logger.exception('exception while stamping %s' % path)
-                pass
+            except SSHException:
+                logger.exception('exception while stamping %s', path)
         end = time.time()
-        logger.debug('stampped {0} dirs, took {1}s'.format(len(self.dirs),
-                                                           str(end-begin)))
+        logger.debug('renewed timestamps on %s dirs, took %ss',
+                     len(self.dirs),
+                     str(end-begin))
+
         ssh.close()
         proxy.close()
+
+    def start(self):
+        self.task.start(self.stamp_interval, now=False)
 
 
 class mirror_site(object):
@@ -69,15 +74,22 @@ class mirror_site(object):
         self.stamps = stamps
         self.check_interval = check_interval
         self.name = name
-        self.diff = -1
+        self.task = task.LoopingCall(self.run)
+        self.diff = {x: -1 for x in self.stamps}
 
     def run(self):
         logger.debug('checking stamps for %s', self.url)
         dirs = utils.get_urlstamps(self.url, self.stamps)
         logger.debug('got stamps for %s\n%s', self.url, self.stamps)
-        for k, v in dirs.iteritems():
-            self.diff = utils.diff_timestamps(utils.get_timestamp(), int(v))
-            logger.debug('%s: %s: out-of-sync: %ss', self.url, k, self.diff)
+        for key, value in dirs.iteritems():
+            self.diff[key] = utils.diff_timestamps(utils.get_timestamp(),
+                                                   value)
+
+    def start(self):
+        self.task.start(self.check_interval, now=False)
+
+    def max_diff(self):
+        return max(v for v in self.diff.itervalues())
 
 
 def setup_logger(log_file, log_level):
@@ -92,82 +104,76 @@ def setup_logger(log_file, log_level):
     log_formatter = ('%(threadName)s::%(levelname)s::%(asctime)s::%(module)s'
                      '::%(lineno)d::%(name)s::(%(funcName)s) %(message)s')
     fmt = logging.Formatter(log_formatter)
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(level)
-    fh.setFormatter(fmt)
+    file_h = logging.FileHandler(log_file)
+    file_h.setLevel(level)
+    file_h.setFormatter(fmt)
     logger.setLevel(level)
-    logger.addHandler(fh)
+    logger.addHandler(file_h)
 
-def generate_dirs(remote_path, amount, ssh_args, ts_fname):
-    dirs = utils.walk_find(remote_path, ssh_args)
-    dirs.sort(key=lambda x: x.count('/'), reverse=True)
-    sample_dirs = random.sample(dirs, min(amount, len(dirs) - 1))
-    return ['%s/%s' % (x.strip(), ts_fname) for x in sample_dirs]
+
+def generate_dirs(dirs, remote_path, amount, ssh_args, ts_fname):
+    if not dirs:
+        dirs = utils.walk_find(remote_path, ssh_args)
+        dirs.sort(key=lambda x: x.count('/'), reverse=True)
+        dirs = random.sample(dirs, min(amount, len(dirs) - 1))
+    return ['%s/%s' % (x.strip(), ts_fname) for x in dirs]
 
 
 def load_config(config_fname):
     global defaults
     configs_yaml = {}
-    with open(config_fname, 'r') as f:
-        configs_yaml = yaml.load(f)
-
+    try:
+        with open(config_fname, 'r') as config_file:
+            configs_yaml = yaml.load(config_file)
+    except IOError:
+        logging.error('failed to open %s, using defaults', config_file)
     configs = defaults.copy()
     configs.update(configs_yaml)
     return configs
 
 
 def build_backend(backend_confs):
-    backend_tasks = {}
-    obj_task = namedtuple('obj_task', 'obj task')
-    backend_obj = stamper(**backend_confs)
-
-
-    if not backend_obj.dirs:
-        backend_stamps = generate_dirs(backend_obj.remote_path,
-                backend_obj.max_stamp,
-                backend_obj.ssh_args,
-                backend_confs.get('ts_fname'))
-        backend_obj.dirs = backend_stamps
-    for mirror in backend_obj.mirrors:
-        mirror_obj = mirror_site(**mirror)
-        mirror_obj.stamps = backend_obj.dirs
-        mirror_task = task.LoopingCall(mirror_obj.run)
-        backend_obj.mirrors_tasks[mirror_obj.name] = obj_task(mirror_obj,
-                                                              mirror_task)
-    backend_task = task.LoopingCall(backend_obj.run)
-    backend_tasks[backend_obj.name] = obj_task(backend_obj, backend_task)
-    return backend_tasks
+    global backend
+    backend = stamper(**backend_confs)
+    backend.dirs = generate_dirs(backend.dirs,
+                                 backend.remote_path,
+                                 backend.max_stamp,
+                                 backend.ssh_args,
+                                 backend.ts_fname)
+    for mirror in backend.mirrors:
+        mirror = mirror_site(stamps=backend.dirs,
+                             **mirror)
+        backend.mirror_tasks[mirror.name] = mirror
+    backend.task = task.LoopingCall(backend.run)
+    return backend
 
 
 @app.route('/mirrors/<mirror_name>', methods=['GET'])
 def get_mirrors(mirror_name):
-    global backends
-    backend = backends.get('resources.ovirt.org')
+    global backend
     if mirror_name == 'all':
         mirrors = []
-        for k, v in backend.obj.mirrors_tasks.iteritems():
-            mirrors.append('%s: %s\n' % (k, v.obj.diff))
+        for name, mirror in backend.mirror_tasks.iteritems():
+            mirrors.append('%s: %s\n' % (name, mirror.max_diff()))
         return ''.join(mirrors)
 
-    mirror = backend.obj.mirrors_tasks.get(mirror_name)
+    mirror = backend.mirror_tasks.get(mirror_name)
     if mirror:
-        return str(mirror.obj.diff)
+        return str(mirror.max_diff())
     else:
-        return 'mirror does not exist'
+        return 'not found'
 
 
 def main():
-    global backends
+    global backend
     config_fname = 'mirrors.yaml'
     configs = load_config(config_fname)
     setup_logger(configs['log_file'], configs['log_level'])
     # to-do: add support for multiply backends
-    backends = build_backend(configs.get('backends')[0])
-    print backends
-    for k, v in backends.iteritems():
-        v.task.start(v.obj.stamp_interval)
-        for k2, v2 in v.obj.mirrors_tasks.iteritems():
-            v2.task.start(v2.obj.check_interval, now=False)
+    backend = build_backend(configs.get('backends')[0])
+    backend.start()
+    for mirror in backend.mirror_tasks.values():
+        mirror.start()
     flask_site = WSGIResource(reactor, reactor.getThreadPool(), app)
     root = Resource()
     root.putChild(configs['http_prefix'], flask_site)
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
+from paramiko.ssh_exception import SSHException
 import paramiko
 import time
 import logging
 import requests
 import re
+import socket
 from datetime import datetime
-logger = logging.getLogger()
+logger = logging.getLogger('__main__')
 def put_sftp(scp, local_file, remote_file, confirm=True):
     scp.put(local_file, remote_file, None, confirm, preserve_mtime=False)
 
@@ -31,19 +33,14 @@ def diff_timestamps(ts1, ts2):
         dt_1 = datetime.fromtimestamp(ts1)
         dt_2 = datetime.fromtimestamp(ts2)
         return (dt_1 - dt_2).total_seconds()
-    except TypeError as e:
+    except TypeError, e:
         logging.exception('illegal timestamp')
         pass
-
-
-
-
 
 def walk_find(remote_path, ssh_args):
     out = []
     cmd = "find {0} -type d -printf '%P\n'".format(remote_path)
     ssh, proxy = get_ssh(ssh_args)
-
     logging.debug('%s', ssh_args)
     stdin, stdout, stderr = ssh.exec_command(cmd, timeout=10)
     out = [line for line in stdout.readlines()]
@@ -57,15 +54,23 @@ def walk_find(remote_path, ssh_args):
 def get_timestamp(format=None):
     return int(time.time())
 
+
 def get_ssh(ssh_args):
     local_sshargs = dict(ssh_args)
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    proxy_cmd = local_sshargs.pop('proxy_cmd', False)
-    ssh_proxy = None
-    if proxy_cmd:
-        proxy_cmd = proxy_cmd.replace('%h', local_sshargs['hostname'])
-        proxy_cmd = proxy_cmd.replace('%p', local_sshargs.get('port', '22'))
-        ssh_proxy = paramiko.ProxyCommand(proxy_cmd)
-    ssh.connect(sock = ssh_proxy, **local_sshargs)
-    return ssh, ssh_proxy if ssh_proxy else type("", (), dict(close=lambda x: None))()
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        proxy_cmd = local_sshargs.pop('proxy_cmd', False)
+        ssh_proxy = None
+        if proxy_cmd:
+            proxy_cmd = proxy_cmd.replace('%h', local_sshargs['hostname'])
+            proxy_cmd = proxy_cmd.replace('%p', local_sshargs.get('port', '22'))
+            ssh_proxy = paramiko.ProxyCommand(proxy_cmd)
+        ssh.connect(sock = ssh_proxy, **local_sshargs)
+        return ssh, ssh_proxy if ssh_proxy else type("", (), dict(close=lambda x: None))()
+    except (socket.gaierror, socket.error, socket.timeout) as err:
+        logger.error('error starting proxy_md %s' % proxy_cmd)
+        raise
+    except SSHException as err:
+        logger.error('error setting up ssh connection')
+        raise
